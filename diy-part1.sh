4074baa45645f05c143fb2e79e28b8fe00d51528
xydche <cherhe@gmail.com>
2024-11-05 11:13:55 +0800

    恢复低版本luci

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,9 @@
 # Add a feed source
 # echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 # echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+sed -i '2s/^#//' feeds.conf.default
+sed -i '3s/^/#/' feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 # git clone https://github.com/sbwml/luci-app-alist package/alist
-git clone https://github.com/rufengsuixing/luci-app-onliner package/onliner
+# git clone https://github.com/rufengsuixing/luci-app-onliner package/onliner
