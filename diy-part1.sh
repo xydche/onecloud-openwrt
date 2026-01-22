bcad1651d39d44e35f7244c80ce1aa9f9eb5feb3
xydche <cherhe@gmail.com>
2026-01-22 14:23:18 +0800

    更新passwall源

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,14 +11,11 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-sed -i '2s/^#//' feeds.conf.default
-sed -i '3s/^/#/' feeds.conf.default
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
+sed -i '2i src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
 
-# git clone -b lua https://github.com/sbwml/luci-app-alist package/alist
-# git clone https://github.com/rufengsuixing/luci-app-onliner package/onliner
+#sed -i '2s/^#//' feeds.conf.default
+#sed -i '3s/^/#/' feeds.conf.default
